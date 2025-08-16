51f4f497ef85635aca2ea4042339ed757d1e91f9
Naruto Sasuke <naruto@hokage.com>
2025-08-16 18:39:56 +0300

    Fix config.py

--- cs-articles/train-chatgpt-on-own-data/src/config.py
+++ cs-articles/train-chatgpt-on-own-data/src/config.py
@@ -5,10 +5,10 @@ OPENAI_API_KEY = "sk-proj-...."
 openai.api_key = OPENAI_API_KEY
 GPT_MODEL = "gpt-4-0613"
 EMBEDDING_MODEL = "text-embedding-ada-002"
-VECTOR_DIR = "../vector_store"
+VECTOR_DIR = "./vector_store"
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 100
 
-ARTICLES_CSV = "../docs/articles.csv"
-PDF_DIR = "../docs/"
+ARTICLES_CSV = "./docs/articles.csv"
+PDF_DIR = "./docs/"
 
